16dc9ec1461a655a742bb9071c23612ceaede99e
wei <wei_wang94@sina.com>
2017-04-16 16:38:54 +0800

    完善 主程序代码

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app/App';
+import './index.css';
+
+// render 是渲染的意思
+ // /> 不加是错误的
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
